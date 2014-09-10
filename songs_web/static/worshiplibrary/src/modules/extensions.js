459ac7a4c67f37b79966fb46bf61e99a38b34989
tim <timford88@gmail.com>
2014-09-10 17:41:34 +0000

    Cleanup extensions and verse modules

--- songs_web/static/worshiplibrary/src/modules/extensions.js
+++ songs_web/static/worshiplibrary/src/modules/extensions.js
@@ -1,25 +1,26 @@
-define(['app', 'handlebars'], function(app, Handlebars){
-	app.module("Extensions", function(Extensions,app,Backbone,Marionette,$,_){
+define(['app', 'handlebars'], function(app, Handlebars) {
+	app.module("Extensions", function(Extensions, app, Backbone, Marionette, $, _) {
 		//modify Backbone.sync to include a csrf token from a cookie if present.
-		app.addInitializer(function(){
-			csrftokenCookieName = 'csrftoken'
+		app.addInitializer(function() {
+			var csrftokenCookieName = 'csrftoken';
 			var oldSync = Backbone.sync;
-			Backbone.sync = function(method, model, options){
-				if (document.cookie && document.cookie !== '') {
-				  var cookies = document.cookie.split(';');
-				  for (var i in cookies) {
-					cookies[i] = $.trim(cookies[i]);
-					if (cookies[i].substring(0, csrftokenCookieName.length + 1) === (csrftokenCookieName + '=')) {
-					  var csrftoken = decodeURIComponent(cookies[i].substring(csrftokenCookieName.length + 1));
-					  var original = options.beforeSend;
-					  options.beforeSend = function(xhr) {
+			Backbone.sync = function(method, model, options) {
+				var original = options.beforeSend,
+					addCSRFToken = function(xhr) {
 						xhr.setRequestHeader('X-CSRFToken', csrftoken);
-						if (original) original();
+						if (original) original(xhr);
 						return true;
-					  };
-					  break;
+					};
+				if (document.cookie && document.cookie !== '') {
+					var cookies = document.cookie.split(';');
+					for (var i in cookies) {
+						cookies[i] = $.trim(cookies[i]);
+						if (cookies[i].substring(0, csrftokenCookieName.length + 1) === (csrftokenCookieName + '=')) {
+							var csrftoken = decodeURIComponent(cookies[i].substring(csrftokenCookieName.length + 1));
+							options.beforeSend = addCSRFToken;
+							break;
+						}
 					}
-				  }
 				}
 				return oldSync(method, model, options);
 			};
@@ -30,4 +31,3 @@ define(['app', 'handlebars'], function(app, Handlebars){
 		});
 	});
 });
-
