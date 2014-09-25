454a013ed380a8806a9461eba2552eb38be73d2e
tim <timford88@gmail.com>
2014-09-25 17:11:14 +0000

    Add models, collections, and views to setlist module

--- songs_web/static/worshiplibrary/src/modules/setlist.js
+++ songs_web/static/worshiplibrary/src/modules/setlist.js
@@ -1,10 +1,12 @@
 define([
 	'app',
 	//Templates
+	'hbs!template/setlist',
+	'hbs!template/setlistarrangement',
 	//Modules
 	'module/extensions'
 	],
-function(app){
+function(app, tplSetlist, tplSetlistArr){
 	app.module("Setlist", function(Setlist,app,Backbone,Marionette,$,_){
 	    Setlist.Model = Backbone.Model.extend({
 	        urlRoot: '/api/setlist/'
@@ -13,6 +15,38 @@ function(app){
 	        url: '/api/setlist/',
 	        model: Setlist.Model
 	    });
+		Setlist.ArrangementModel = Backbone.Model.extend({
+			urlRoot: '/api/setlist/arrangement/'
+		});
+		Setlist.ArrangementCollection = Backbone.Collection.extend({
+			url: '/api/setlist/arrangement/',
+			model: Setlist.ArrangementModel
+		});
+		Setlist.ArrangementView = Backbone.Marionette.ItemView.extend({
+			tagName: 'tr',
+			template: tplSetlistArr
+		});
+		Setlist.CompositeView = Backbone.Marionette.CompositeView.extend({
+			template: tplSetlist,
+			childView: app.Setlist.ArrangementView,
+			childViewContainer:'tbody',
+			onTabAdd: function(){
+				this.collection = new app.Setlist.ArrangementCollection(this.model.get('setlist_arrangements'));
+				this.render();
+			}
+		});
+		
+		setlist = new Setlist.Model({id:1});
+		setlist.fetch({
+			success:function(sl){
+				app.setlistTabManager.addTab({
+					title: sl.get('description'),
+					model: sl
+				}).showTab({
+					id:sl.get('id')
+				});
+			}
+		});
 	    
 	});
 });
