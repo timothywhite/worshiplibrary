e9005c3339179274aae49bdb27a315805ece0776
tim <timford88@gmail.com>
2014-09-25 17:08:37 +0000

    Add description and notes fields to setlistarrangement api

--- songs_api/serializers/setlist.py
+++ songs_api/serializers/setlist.py
@@ -2,9 +2,11 @@ from songs_api.models import SetList, SetListArrangement
 from rest_framework import serializers
 
 class SetListArrangementSerializer(serializers.ModelSerializer):
+	description = serializers.CharField(max_length=255, source='arrangement.description', read_only=True)
+	notes = serializers.CharField(max_length=255, source='arrangement.notes', read_only=True)
 	class Meta:
 		model = SetListArrangement
-		fields = ('id', 'order', 'arrangement')
+		fields = ('id', 'order', 'arrangement', 'description', 'notes')
 		
 class SetListSerializer(serializers.ModelSerializer):
 	setlist_arrangements = SetListArrangementSerializer(many=True,read_only=True)
